cf674f84977497ac93ffac3e31560afa63cb89be
Dev Ramesh <dev_ramesh@brown.edu>
2022-06-28 22:42:23 -0400

    Visual Changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import './expense.dart';
+import 'dart:math';
+import 'package:intl/intl.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -31,24 +34,45 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final List<Expense> _expenses = [];
 
-List<Expense> get _allExpenses {
+  // list of users expenses
+  final List<Expense> _userExpenses = [];
+
+  // creation of list of example expenses to test scroll functionality
+  List<Expense> get _exampleExpenses {
+  
     List<Expense> expenseList = [];
 
-    int i = 0;
+    // ids start after _userExpenses ids
+    int i = _userExpenses.length + 1;
+    Random rnd = Random();
+    DateTime date = DateTime.now();
+    
+    // while loop to create X amount of transactions
     while (i < 1000) {
-      expenseList.add(Expense(
-      id: i,
-      cost: 17.20,
-      note: 'Expense ${i.toString()}',
-      date: DateTime.now(),
-    ),);
+      // creation of example expenses with random costs less than $100
+      expenseList.add(
+        Expense(
+          id: i,
+          cost: (1 + rnd.nextInt(10000)).toDouble()/100,
+          note: 'Expense ${i.toString()}',
+          date: date,
+        ),
+      );
     i++;
+    // subtracts date by one for next example expense
+    date = date.subtract(const Duration(days: 1));
     }
 
     return expenseList;
   }
+
+  // returns expenses list with _userExpenses and _exampleExpenses combined 
+  // (remove _exampleExpenses from this statement if you don't want them)
+  List<Expense> get _expenses {
+    return _userExpenses + _exampleExpenses;
+  }
+
 
 
   @override
@@ -59,37 +83,54 @@ List<Expense> get _allExpenses {
       ),
       body: SingleChildScrollView(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'Your Expenses:',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 30
-                ),
-            ),
             Container(
+              margin: const EdgeInsets.only(top: 10.0, bottom: 5),
+              // title of 'Your Expenses'
+              child: const Text(
+                'Your Expenses:',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30
+                  ),
+              ),
+            ),
+            SizedBox(
               height: 500,
+              // lazy load implementation to loop through all expenses
               child: ListView.builder(
+                // builder for individual expense
                 itemBuilder: (BuildContext context, int index) {
-                  return Text(
-                    '${_allExpenses[index].note.toString()}: ${_allExpenses[index].cost.toString()}, ${_allExpenses[index].date.toString()}\n',
-                    style: TextStyle(
-                          fontSize: 20
-                          ),
-                  );
+                  // card around expenses
+                  return  Card(
+                  // adds drop shadow
+                  elevation: 5,
+                  child: Container(
+                    alignment: Alignment.center, 
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 20,
+                      horizontal: 5,
+                    ),
+                    child: Text(
+                      '${_expenses[index].note.toString()}: \$${_expenses[index].cost.toString()}, ${  DateFormat.yMd().format(_expenses[index].date) }',
+                      style: const TextStyle(
+                            fontSize: 18
+                            ),
+
+                    ),
+                  ));
+                  
                 },
-                itemCount: _allExpenses.length,
+                itemCount: _expenses.length,
               ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
+      floatingActionButton: const FloatingActionButton(
         onPressed: null,
         tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
